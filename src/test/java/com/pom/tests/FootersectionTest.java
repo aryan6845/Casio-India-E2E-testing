0d563448ec672d254a7b009e893bc05d2a9820f3
Aryan <kumarsingharyan745@gmail.com>
2025-05-07 12:22:54 +0000

    Bug_fix

--- src/test/java/com/pom/tests/FootersectionTest.java
+++ src/test/java/com/pom/tests/FootersectionTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+// nyc
 import com.pom.Pages.FootersectionPage;
 
 public class FootersectionTest {
